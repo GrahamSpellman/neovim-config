0f988342b08d0ee5b83055ad92a5c47430a5c3bc
GrahamSpellman <graham.spellman99@gmail.com>
2025-07-14 10:31:27 -0400

    binds for emacs browser

--- lua/grams/core/remap.lua
+++ lua/grams/core/remap.lua
@@ -7,8 +7,8 @@ keymap.set("n", "<leader>pv", vim.cmd.Ex)
 keymap.set("n", "<leader>M", ":pu_<Enter><esc>")
 keymap.set("n", "<leader>m", ":-pu_<Enter><esc>")
 
-
-
+-- Telescope
+keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
 
 
 

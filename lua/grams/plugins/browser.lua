67a9d3850603e42da5d6c16d9cf423a9992c32a7
GrahamSpellman <graham.spellman99@gmail.com>
2025-07-17 13:25:19 -0400

    save

--- lua/grams/plugins/browser.lua
+++ lua/grams/plugins/browser.lua
@@ -3,14 +3,12 @@ return
         "nvim-telescope/telescope-file-browser.nvim",
         dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
         config = function()
-            -- You don't need to set any of these options.
-            -- IMPORTANT!: this is only a showcase of how you can set default options!
             require("telescope").setup {
                 extensions = {
                     file_browser = {
                         theme = "ivy",
-                        -- disables netrw and use telescope-file-browser in its place
-                        hijack_netrw = true,
+                        hijack_netrw = false,
+                        initial_mode = "normal",
                         mappings = {
                             ["i"] = {
                                 -- your custom insert mode mappings
